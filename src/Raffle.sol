84cd00215f53bf22b0e78cbc6e101cab0860c798
samuelfrommars <connectwithayeni@gmail.com>
2024-09-18 06:09:47 +0100

    first commit for raffle smart contract

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+ * @title A sample raflle contract for learning purposes
+ * @author Ayeni Samuel
+ * @notice This contract is a learning material to create a sample raffle
+ * @dev Implements ChainLink VRFv2.5
+ */
+
+contract Raffle {
+    constructor() {}
+}
