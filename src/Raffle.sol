44dcd0345bd8180e52a2730083bb2b1a84d6b160
samuelfrommars <connectwithayeni@gmail.com>
2024-09-18 23:24:37 +0100

    chore: updated enter raffle function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -36,24 +36,31 @@ contract Raffle {
 
     /* Variables */
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    /* Events */
+    event RaffleEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
     }
 
     /// Enter the address into the Raffle contract
     // TODO: write test ensuring that revert is called when the entranceFee is lower than the allowed entrance fee
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreEthToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+        // Makes front end indexing faster
+        emit RaffleEntered(msg.sender);
     }
 
     /// Pick the winner of the raffle and send the winner the amount won
     // TODO: write test ensuring that the winner is selected at random
     // TODO: write test ensuring that the selected winner is paid all the raffle amount
-    function pickWinner() public {}
+    function pickWinner() external {}
 
     /// Get the entrance fee to participate in the raffle
     function getEntranceFee() external view returns (uint256) {
@@ -62,5 +69,9 @@ contract Raffle {
 
     function getPlayersNumber() external view returns (uint256) {
         return s_players.length;
+    }
+
+    function getRaffleInterval() external view returns (uint256) {
+        return i_interval;
     }
 }
--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -10,6 +10,7 @@ contract RaffleTest is Test {
     Raffle raffle;
     uint256 constant ENTRANCE_AMOUNT = 0.001 ether;
     uint256 constant FAILED_AMOUNT = 0.0001 ether;
+    uint256 constant VALID_AMOUNT = 0.0015 ether;
 
     function setUp() external {
         raffle = new Raffle(ENTRANCE_AMOUNT);
@@ -19,5 +20,13 @@ contract RaffleTest is Test {
     function testEnterValueAgainstEntraceAmount() public {
         vm.expectRevert();
         raffle.enterRaffle{value: FAILED_AMOUNT}();
+    }
+
+    /// SHOULD test that player list lenght increases after entering raffle
+    function testThatPlayerListIsUpdated() public {
+        uint256 initialLength = raffle.getPlayersNumber();
+        raffle.enterRaffle{value: VALID_AMOUNT}();
+        uint256 updatedLength = raffle.getPlayersNumber();
+        assertEq(updatedLength, initialLength + 1);
     }
 }
