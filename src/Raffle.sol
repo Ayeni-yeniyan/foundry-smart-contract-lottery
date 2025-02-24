4f0aef10c17d4fa7d03c318fad288861258682cf
Ayeni-yeniyan <connectwithayeni@gmail.com>
2025-02-24 01:21:46 +0100

    feat: re previous commit

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -21,6 +21,8 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
  * @title A sample raffle contract for learning purposes
@@ -30,20 +32,54 @@ pragma solidity 0.8.19;
  *
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /* Errors */
     error Raffle__SendMoreEthToEnterRaffle();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState);
+
+    /* Enums */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
 
     /* Variables */
+
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private constant NUM_WORDS = 3;
     uint256 private immutable i_entranceFee;
+    // @dev duration of lottery
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyhash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
+    address private s_recentWinner;
+    uint256 private s_lastTimeStamp;
+    RaffleState private s_raffleState;
+
     /* Events */
     event RaffleEntered(address indexed player);
+    event WinnerPicked(address indexed winner);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        i_keyhash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     /// Enter the address into the Raffle contract
@@ -52,15 +88,48 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreEthToEnterRaffle();
         }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
         s_players.push(payable(msg.sender));
         // Makes front end indexing faster
         emit RaffleEntered(msg.sender);
     }
 
+
+    function checkUpkeep(bytes memory /** v */) public view  returns (
+        bool upkeepNeeded,bytes memory /* PerformData */)  {
+         bool timeHasPassed=(block.timestamp - s_lastTimeStamp) < i_interval;
+         bool isOpen=s_raffleState==RaffleState.OPEN;
+         bool hasBalance =address(this).balance>0;
+         bool hasPlayers =s_players.length>0;
+upkeepNeeded=timeHasPassed&&isOpen&&hasBalance&&hasPlayers;
+         return (upkeepNeeded,"");
+    }
+
     /// Pick the winner of the raffle and send the winner the amount won
     // TODO: write test ensuring that the winner is selected at random
     // TODO: write test ensuring that the selected winner is paid all the raffle amount
-    function pickWinner() external {}
+    function pickWinner(bytes calldata /** v */ x) external {
+        (bool upkeedNeeded,)= checkUpkeep("");
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert Raffle__UpkeepNotNeeded(address(this).balance,s_players.length,uint256(s_raffleState));
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyhash, // Replace with actual keyHash
+                subId: 0, // Replace with actual subscription ID
+                requestConfirmations: REQUEST_CONFIRMATION, // Replace with actual minimum request confirmations
+                callbackGasLimit: i_callbackGasLimit, // Replace with actual callback gas limit
+                numWords: NUM_WORDS, // Replace with actual number of words
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+        // Get random number from chainlink vrf
+    }
 
     /// Get the entrance fee to participate in the raffle
     function getEntranceFee() external view returns (uint256) {
@@ -74,4 +143,23 @@ contract Raffle {
     function getRaffleInterval() external view returns (uint256) {
         return i_interval;
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(s_recentWinner);
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 }
+
+// CEIs Checks, Effects and Interactions
--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -3,30 +3,19 @@ pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 
-import {Raffle} from "../../src/Raffle.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
 
 /// Test contract for Raffle contract
 contract RaffleTest is Test {
-    Raffle raffle;
-    uint256 constant ENTRANCE_AMOUNT = 0.001 ether;
-    uint256 constant FAILED_AMOUNT = 0.0001 ether;
-    uint256 constant VALID_AMOUNT = 0.0015 ether;
+    Raffle public raffle;
+    HelperConfig public helperConfig;
+    address public PLAYER = makeAddr("Player");
+    uint256 constant PLAYER_STARTING_BALANCE = 10 ether;
 
     function setUp() external {
-        raffle = new Raffle(ENTRANCE_AMOUNT);
-    }
-
-    /// SHOULD revert if sent value is less than entrance amount
-    function testEnterValueAgainstEntraceAmount() public {
-        vm.expectRevert();
-        raffle.enterRaffle{value: FAILED_AMOUNT}();
-    }
-
-    /// SHOULD test that player list lenght increases after entering raffle
-    function testThatPlayerListIsUpdated() public {
-        uint256 initialLength = raffle.getPlayersNumber();
-        raffle.enterRaffle{value: VALID_AMOUNT}();
-        uint256 updatedLength = raffle.getPlayersNumber();
-        assertEq(updatedLength, initialLength + 1);
+        DeployRaffle deployer = new DeployRaffle();
+        (raffle, ) = deployer.deployContract();
     }
 }
